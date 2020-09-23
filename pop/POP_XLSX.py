7828c989b71b72b092addf0740cba3f8dfd06c2e
wanyingchao <1063983073@qq.com>
2020-09-23 16:28:38 +0800

    增加单独孔位弹出

--- pop/POP_XLSX.py
+++ pop/POP_XLSX.py
@@ -1,22 +1,17 @@
-import xlrd
 import requests
 import datetime
-
-file = 'E:\python\pop\pop.xlsx'
-wb = xlrd.open_workbook(filename=file)
-sheet = wb.sheet_by_index(0)
-cols = sheet.col_values(0)
-file_logs = open('E:\python\pop\POP_LOGS.txt', 'a', encoding='utf8')
+import xlrd
 url = 'https://www.jzcdsc.com/charge/device/pop'
+file_logs = open('E:\python\pop\POP_LOGS.txt', 'a', encoding='utf8')
 
 
-def pop():
+def pop(deviceid, slot):
     par = {
         'deviceid': deviceid,
         'slot': slot
     }
     body = {
-        'Content Type': 'application.json;charset=utf-8',
+        'Content-Type': 'application/json;charset=utf-8',
     }
     r = requests.post(url, par, body, verify=True)
     result = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + ' ' + deviceid + " " + str(slot) + "口" + r.text
@@ -24,15 +19,29 @@ def pop():
     file_logs.write(result + '\n')
 
 
-for deviceid in cols:
-    if deviceid[4] + deviceid[5] == '06':
-        for slot in range(1, 7):
-            pop()
-    elif deviceid[4] + deviceid[5] == '09':
-        for slot in range(1, 10):
-            pop()
-    else:
-        for slot in range(1, 13):
-            pop()
+def pop_one(deviceid, slots):
+    for slot in slots:
+        pop(deviceid, slot)
+
+
+def pop_all():
+    file = 'E:\python\pop\pop.xlsx'
+    wb = xlrd.open_workbook(filename=file)
+    sheet = wb.sheet_by_index(0)
+    cols = sheet.col_values(0)
+    for deviceid in cols:
+        if deviceid[4] + deviceid[5] == '06':
+            for slot in range(1, 7):
+                pop(deviceid, slot)
+        elif deviceid[4] + deviceid[5] == '09':
+            for slot in range(1, 10):
+                pop(deviceid, slot)
+        else:
+            for slot in range(1, 13):
+                pop(deviceid, slot)
+
+
+# pop_one('JZCB061904001009', '24')
+pop_all()
 
 file_logs.close()

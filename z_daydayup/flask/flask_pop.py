fd4e08647e5ad0c5eb2e61ec9b4408f94275d55a
wanyingchao <1063983073@qq.com>
2020-10-21 11:56:31 +0800

    输出日志，优化代码结构

--- z_daydayup/flask/flask_pop.py
+++ z_daydayup/flask/flask_pop.py
@@ -4,12 +4,13 @@ import xlrd
 import requests
 import datetime
 from flask import render_template
+from log.logger import *
 url = 'https://www.jzcdsc.com/charge/device/pop'
 interface_path = os.path.dirname(__file__)
-sys.path.insert(0, interface_path)
+sys.path.insert(0, interface_path)  # 将当前文件的父目录加入临时系统变量
 
 
-def pop(deviceid, slot, text):
+def pop_flask(deviceid, slot, text):
     par = {
         'deviceid': deviceid,
         'slot': slot
@@ -20,10 +21,30 @@ def pop(deviceid, slot, text):
     r = requests.post(url, par, body, verify=True)
     result = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + ' ' + deviceid + " " + str(
         slot) + "口" + r.text
+    logger.info(result)
     text.append(result)
 
 
+def pop_loop(deviceid, text):
+    if deviceid[4] + deviceid[5] == '06':
+        for slot in range(1, 7):
+            pop_flask(deviceid, slot, text)
+            if slot == 6:
+                text.append('---------------------------------------------------------------------------------------')
+    elif deviceid[4] + deviceid[5] == '09':
+        for slot in range(1, 10):
+            pop_flask(deviceid, slot, text)
+            if slot == 9:
+                text.append('---------------------------------------------------------------------------------------')
+    else:
+        for slot in range(1, 13):
+            pop_flask(deviceid, slot, text)
+            if slot == 12:
+                text.append('---------------------------------------------------------------------------------------')
+
+
 server = Flask(__name__, static_folder='static')  # Flask必须创建程序实例，Flask类只有一个必须指定的参数，即程序主模块或者包的名字，__name__是系统变量，该变量指的是本py文件的文件名
+# 需要自己在同级 创建一个名为 static 的文件夹来存放上传的文件
 
 
 @server.route('/pop', methods=['post'])  # 文件路径/pop，方法post
@@ -38,48 +59,20 @@ def pop_all():
         sheet = wb.sheet_by_index(0)
         cols = sheet.col_values(0)
         for deviceid in cols:
-            if deviceid[4] + deviceid[5] == '06':
-                for slot in range(1, 7):
-                    pop(deviceid, slot, text)
-                    if slot == 6:
-                        text.append('---------------------------------------------------------------------------------------')
-            elif deviceid[4] + deviceid[5] == '09':
-                for slot in range(1, 10):
-                    pop(deviceid, slot, text)
-                    if slot == 9:
-                        text.append('---------------------------------------------------------------------------------------')
-            else:
-                for slot in range(1, 13):
-                    pop(deviceid, slot, text)
-                    if slot == 12:
-                        text.append('---------------------------------------------------------------------------------------')
+            pop_loop(deviceid, text)
         return render_template('index.html', text=text)
     else:
         text = []
         slots_list = request.form['slot']
         deviceid = request.form['data']
-    if len(slots_list) == 0:
-        if deviceid[4] + deviceid[5] == '06':
-            for slot in range(1, 7):
-                pop(deviceid, slot, text)
-                if slot == 6:
-                    text.append('---------------------------------------------------------------------------------------')
-        elif deviceid[4] + deviceid[5] == '09':
-            for slot in range(1, 10):
-                pop(deviceid, slot, text)
-                if slot == 9:
-                    text.append('---------------------------------------------------------------------------------------')
+        if len(slots_list) == 0:
+            pop_loop(deviceid, text)
         else:
-            for slot in range(1, 13):
-                pop(deviceid, slot, text)
-                if slot == 12:
-                    text.append('---------------------------------------------------------------------------------------')
-
-    else:
-        slots = slots_list.split(',')
-        for slot in slots:
-            pop(deviceid, slot, text)
-    return render_template('index.html', text=text)
+            slots = slots_list.split(',')
+            for slot in slots:
+                pop_flask(deviceid, slot, text)
+        # 需要在同级目录创建templates文件存放index.html
+        return render_template('index.html', text=text)
 
 
 server.run(host='192.168.0.93', port=8888)
---
+++ z_daydayup/flask/log/logger.py
@@ -0,0 +1,28 @@
+import logging
+
+
+def get_log():
+    # 配置日志参数
+    logger = logging.getLogger()
+    # 设置日志级别
+    logger.setLevel(logging.INFO)
+    # 日志文件保存路径
+    logfile = 'E://python/z_daydayup/flask/log/logs.log'
+    # 日志输出到文件
+    fh = logging.FileHandler(logfile)
+    fh.setLevel(logging.INFO)
+    # 日志输出到控制台
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    # 设置日志输出格式
+    formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s:%(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    return logger
+
+
+logger = get_log()
